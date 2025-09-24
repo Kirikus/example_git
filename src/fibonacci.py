bb42d2882969b5981c02a283f267544f2274953c
Kirikus <kirikusan@gmail.com>
2025-09-24 16:12:18 +0300

    Code (#5)
    * Code: fixed most ruff errors
    
     - added docstrings
     - fixed naming
     - fixed if clauses
    
    * Code: renamed to fibonacci.py
    
    A005 Module `code` shadows a Python standard-library module

---
+++ src/fibonacci.py
@@ -0,0 +1,30 @@
+"""Fibonacci series module."""
+
+
+
+def fibonacci(n: int) -> int:
+    """Fibonacci series.
+
+    :param n: fibonacci number to return.
+    :return: n-th fib. number.
+    """
+    a = 0
+    b = 1
+
+    if n < 0:
+        print("Incorrect input")
+
+    elif n == 0:
+        return 0
+
+    elif n == 1:
+        return b
+    for _ in range(1, n):
+        c = a + b
+        a = b
+        b = c
+    return b
+
+
+if __name__ == "__main__":
+    print(fibonacci(9))
